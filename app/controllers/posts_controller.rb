c7cffc2dfa7d4383b10cb848d8decbdd800ef3c0
Irum <irum@Irums-MacBook-Air.local>
2018-10-14 15:08:43 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,12 @@ class PostsController < ApplicationController
 
   def update
     @post.update(post_params)
-
-    redirect_to post_path(@post)
-  end
+      if @post.save
+        redirect_to post_path(@post)
+      else
+        render :edit
+      end
+    end
 
   private
 
